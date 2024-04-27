108bf6a11bc965979b5d422cb807686562a6c789
Emanuel <101946589+Emanuel3queijos@users.noreply.github.com>
2024-04-27 02:45:35 -0300

    backend ok

---
+++ backend/housing-location-project-backend/src/main/java/com/br/emanuelap/housinglocationprojectbackend/HousingLocationProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.br.emanuelap.housinglocationprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousingLocationProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HousingLocationProjectBackendApplication.class, args);
+    }
+
+}
---
+++ backend/housing-location-project-backend/src/test/java/com/br/emanuelap/housinglocationprojectbackend/HousingLocationProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.emanuelap.housinglocationprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousingLocationProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
